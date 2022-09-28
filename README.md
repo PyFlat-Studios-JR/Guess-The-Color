65b644c065019ef31271ab77226ffd4f628c8db3
Anonym2546 <johannes.25406@gmail.com>
2022-09-28 21:45:29 +0200

    ⠀

---
+++ README.md
@@ -0,0 +1,9 @@
+# Requirements:
+- (Python 3)(You can just Run Guess The Color.exe)
+- (Pip)
+
+# Run:
+- (Move the Un-Zipped Folder to your Favourite location)
+- (Double Click Start.bat)
+- Execute Guess The Color.exe
+
---
+++ files/run.bat
@@ -0,0 +1,11 @@
+echo off
+title Guess the Color Console
+cls
+echo **INFO**
+echo This Screen has no meaning, just minimize it
+echo Running Guess The Color v.1.0
+python guess_the_color.py
+cls
+echo Process terminated
+pause
+exit
